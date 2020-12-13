7226247baa43345ec22248608a356b293aba558a
max.jian <sibo_jian_29903549@163.com>
2020-12-13 23:58:33 +0800

    some change

--- Sources/Core/VMBannerCollectionViewCell.swift
+++ Sources/Core/VMBannerCollectionViewCell.swift
@@ -5,8 +5,12 @@
 //  Created by max on 2020/10/23.
 //
 
+#if canImport(UIKit)
+
 import UIKit
 
 class VMBannerCollectionViewCell: UICollectionViewCell, VMBannerViewItemProtocol {
     
 }
+
+#endif
