c02df15893dd7782383daf3d1d46e85e5c042da6
max.jian <sibo_jian_29903549@163.com>
2020-12-13 23:58:25 +0800

    add VMBannerViewTransformer empty file

---
+++ Sources/Core/VMBannerViewTransformer.swift
@@ -0,0 +1,17 @@
+//
+//  VMBannerViewTransformer.swift
+//  Champi
+//
+//  Created by max on 2020/12/13.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public class VMBannerViewTransformer: NSObject {
+
+  open internal(set) weak var bannerView: VMBannerView?
+}
+
+#endif
