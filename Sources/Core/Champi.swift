fb8ec438822f9d38c247e60961cae6a5072679df
max.jian <sibo_jian_29903549@163.com>
2020-12-13 23:57:59 +0800

    add VMBannerViewDataSource & VMBannerViewDelegate method & add VMBannerView code

--- Sources/Core/Champi.swift
+++ Sources/Core/Champi.swift
@@ -27,11 +27,25 @@ public enum VMBannerViewTransformerType: Int {
 }
 
 @objc public protocol VMBannerViewDataSource: NSObjectProtocol {
-
+  func numberOfItems(in bannerView: VMBannerView) -> Int
+  func bannerView(_ bannerView: VMBannerView, cellForItemAt index: Int) -> VMBannerViewItemProtocol
 }
 
 @objc public protocol VMBannerViewDelegate: NSObjectProtocol {
+  @objc optional func bannerView(_ bannerView: VMBannerView, shouldHighlightItemAt index: Int) -> Bool
+  @objc optional func bannerView(_ bannerView: VMBannerView, didHighlightItemAt index: Int)
   
+  @objc optional func bannerView(_ bannerView: VMBannerView, shouldSelectItemAt index: Int) -> Bool
+  @objc optional func bannerView(_ bannerView: VMBannerView, didSelectItemAt index: Int)
+  
+  @objc optional func bannerView(_ bannerView: VMBannerView, willDisplay cell: VMBannerViewItemProtocol, forItemAt index: Int)
+  @objc optional func bannerView(_ bannerView: VMBannerView, didEndDisplaying cell: VMBannerViewItemProtocol, forItemAt index: Int)
+  
+  @objc optional func bannerViewDidScroll(_ bannerView: VMBannerView)
+  @objc optional func bannerViewWillBeginDragging(_ bannerView: VMBannerView)
+  @objc optional func bannerViewWillEndDragging(_ bannerView: VMBannerView, targetIndex: Int)
+  @objc optional func bannerViewDidEndDecelerating(_ bannerView: VMBannerView)
+  @objc optional func bannerViewDidEndScrollingAnimation(_ bannerView: VMBannerView)
 }
 
 #endif
--- Sources/Core/VMBannerView.swift
+++ Sources/Core/VMBannerView.swift
@@ -5,37 +5,77 @@
 //  Created by max on 2020/10/23.
 //
 
+#if canImport(UIKit)
+
 import UIKit
 
+@IBDesignable
 public class VMBannerView: UIView {
-    
-  public var bounces: Bool {
-    set {
-      self.collectionView.bounces = newValue
+  
+  @IBOutlet public weak var dataSource: VMBannerViewDataSource?
+  @IBOutlet public weak var delegate: VMBannerViewDelegate?
+  
+  @IBInspectable
+  public var interitemSpacing: CGFloat = 0 {
+    didSet {
+      self.collectionViewLayout.forceInvalidate()
     }
+  }
+  
+  @IBInspectable
+  public var itemSize: CGSize = VMBannerView.automaticSize {
+    didSet {
+      self.collectionViewLayout.forceInvalidate()
+    }
+  }
+  
+  @IBInspectable
+  public var decelerationDistance: UInt = 1
+  
+  @IBInspectable
+  public var backgroundView: UIView? {
+    didSet {
+      if let backgroundView = self.backgroundView {
+        if backgroundView.superview != nil {
+          backgroundView.removeFromSuperview()
+        }
+        self.insertSubview(backgroundView, at: 0)
+        self.setNeedsLayout()
+      }
+    }
+  }
+  
+  @IBInspectable
+  public var bounces: Bool {
     get {
       return self.collectionView.bounces
     }
+    set {
+      self.collectionView.bounces = newValue
+    }
   }
   
+  @IBInspectable
   public var alwaysBounceVertical: Bool {
-    set {
-      self.collectionView.alwaysBounceVertical = newValue
-    }
     get {
       return self.collectionView.alwaysBounceVertical
     }
+    set {
+      self.collectionView.alwaysBounceVertical = newValue
+    }
   }
   
+  @IBInspectable
   public var alwaysBounceHorizontal: Bool {
-    set {
-      self.collectionView.alwaysBounceHorizontal = newValue
-    }
     get {
       return self.collectionView.alwaysBounceHorizontal
     }
+    set {
+      self.collectionView.alwaysBounceHorizontal = newValue
+    }
   }
   
+  @IBInspectable
   public var isScrollEnabled: Bool {
     set {
       self.collectionView.isScrollEnabled = newValue
@@ -45,8 +85,7 @@ public class VMBannerView: UIView {
     }
   }
   
-  public var scrollDirection: VMBannerView.ScrollDirection = .horizontal
-  
+  @IBInspectable
   public var automaticSlidingInterval: TimeInterval = .zero {
     didSet {
       self.cancelTimer()
@@ -56,13 +95,102 @@ public class VMBannerView: UIView {
     }
   }
   
-  private weak var contentView: UIView!
-  private weak var collectionViewLayout: VMBannerCollectionViewLayout!
-  private weak var collectionView: VMBannerCollectionView!
+  @IBInspectable
+  public var removeInfiniteLoopForSingleItem: Bool = false {
+    didSet {
+      self.reloadData()
+    }
+  }
+  
+  @IBInspectable
+  public var isInfinite: Bool = false {
+    didSet {
+      self.collectionViewLayout.needsReprepare = true
+      self.collectionView.reloadData()
+    }
+  }
+  
+  public var scrollDirection: VMBannerView.ScrollDirection = .horizontal {
+    didSet {
+      self.collectionViewLayout.forceInvalidate()
+    }
+  }
+  
+  public var transformer: VMBannerViewTransformer? {
+    didSet {
+      self.transformer?.bannerView = self
+      self.collectionViewLayout.forceInvalidate()
+    }
+  }
+  
+  public var isTracking: Bool {
+    return self.collectionView.isTracking
+  }
+  
+  public var scrollOffset: CGFloat {
+    let contentOffset = max(self.collectionView.contentOffset.x, self.collectionView.contentOffset.y)
+    let scrollOffset = CGFloat(contentOffset / self.collectionViewLayout.itemSpacing)
+    return fmod(scrollOffset, CGFloat(self.numberOfItems))
+  }
+  
+  public var panGestureRecognizer: UIPanGestureRecognizer {
+    return self.collectionView.panGestureRecognizer
+  }
+  
+  public private(set) var currentIndex = 0
+  
+  internal weak var contentView: UIView!
+  internal weak var collectionViewLayout: VMBannerCollectionViewLayout!
+  internal weak var collectionView: VMBannerCollectionView!
+  
+  internal var timer: Timer?
+  
+  internal var numberOfItems: Int = 0
+  internal var numberOfSections: Int = 0
   
   private var dequeueSection = 0
+  private var centermostIndexPath: IndexPath {
+    guard self.numberOfItems > 0, self.collectionView.contentSize != .zero else {
+      return IndexPath(item: 0, section: 0)
+    }
+    
+    let sortedIndexPaths = self.collectionView.indexPathsForVisibleItems.sorted(by: { (lhs, rhs) -> Bool in
+      let leftFrame = self.collectionViewLayout.frame(for: lhs)
+      let rightFrame = self.collectionViewLayout.frame(for: rhs)
+      
+      var leftCenter: CGFloat
+      var rightCenter: CGFloat
+      var ruler: CGFloat
+      
+      switch self.scrollDirection {
+      case .horizontal:
+        leftCenter = leftFrame.midX
+        rightCenter = rightFrame.midX
+        ruler = self.collectionView.bounds.midX
+      case .vertical:
+        leftCenter = leftFrame.midY
+        rightCenter = rightFrame.midY
+        ruler = self.collectionView.bounds.midY
+      }
+      
+      return abs(ruler - leftCenter) < abs(ruler - rightCenter)
+    })
+    
+    guard let indexPath = sortedIndexPaths.first else {
+      return IndexPath(item: 0, section: 0)
+    }
+    
+    return indexPath
+  }
   
-  private var timer: Timer?
+  private var isPossibleRotating: Bool {
+    guard let animationKeys = self.contentView.layer.animationKeys() else {
+      return false
+    }
+    
+    return animationKeys.contains(where: { ["position", "bounds.origin", "bounds.size"].contains($0) })
+  }
+  private var possibleTargetIndexPath: IndexPath?
   
   public override init(frame: CGRect) {
     super.init(frame: frame)
@@ -82,6 +210,7 @@ public class VMBannerView: UIView {
   public override func layoutSubviews() {
     super.layoutSubviews()
     
+    self.backgroundView?.frame = self.bounds
     self.contentView.frame = self.bounds
     self.collectionView.frame = self.bounds
   }
@@ -113,13 +242,13 @@ public class VMBannerView: UIView {
   }
   
   public func selectItem(at index: Int, animated: Bool) {
-    let indexPath = self.nearbyIndexPath(index)
+    let indexPath = self.nearbyIndexPath(for: index)
     let scrollPosition: UICollectionView.ScrollPosition = self.scrollDirection == .horizontal ? .centeredHorizontally : .centeredVertically
     self.collectionView.selectItem(at: indexPath, animated: animated, scrollPosition: scrollPosition)
   }
   
   public func deselectItem(at index: Int, animated: Bool) {
-    let indexPath = self.nearbyIndexPath(index)
+    let indexPath = self.nearbyIndexPath(for: index)
     self.collectionView.deselectItem(at: indexPath, animated: animated)
   }
   
@@ -131,8 +260,27 @@ public class VMBannerView: UIView {
   }
   
   public func cellForItem(at index: Int) -> VMBannerViewItemProtocol? {
-    let indexPath = self.nearbyIndexPath(index)
+    let indexPath = self.nearbyIndexPath(for: index)
     return self.collectionView.cellForItem(at: indexPath) as? VMBannerViewItemProtocol
+  }
+  
+  public func scrollToItem(at index: Int, animated: Bool) {
+    guard index < self.numberOfItems else {
+      fatalError("index \(index) is out of range [0...\(self.numberOfItems - 1)]")
+    }
+    
+    let indexPath: IndexPath = {
+      if let indexPath = self.possibleTargetIndexPath, indexPath.item == index {
+        defer {
+          self.possibleTargetIndexPath = nil
+        }
+        return indexPath
+      }
+      return self.numberOfSections > 1 ? self.nearbyIndexPath(for: index) : IndexPath(item: index, section: 0)
+    }()
+    
+    let contentOffset = self.collectionViewLayout.contentOffset(for: indexPath)
+    self.collectionView.setContentOffset(contentOffset, animated: animated)
   }
   
   private func initialize() {
@@ -169,21 +317,183 @@ public class VMBannerView: UIView {
     self.timer = nil
   }
   
-  private func nearbyIndexPath(_ index: Int) -> IndexPath {
-    return nil
+  private func nearbyIndexPath(for index: Int) -> IndexPath {
+    let currentIndex = self.currentIndex
+    let currentSection = self.centermostIndexPath.section
+    
+    if abs(currentIndex - index) <= self.numberOfItems / 2 {
+      return IndexPath(item: index, section: currentSection)
+    }
+    else if index - currentIndex >= 0 {
+      return IndexPath(item: index, section: currentSection - 1)
+    }
+    else {
+      return IndexPath(item: index, section: currentSection + 1)
+    }
   }
   
   @objc private func slidingNext(_ timer: Timer?) {
+    guard let _ = self.superview, let _ = self.window, self.numberOfItems > 0, !self.isTracking else {
+      return
+    }
     
+    let contentOffset: CGPoint = {
+      let indexPath = self.centermostIndexPath
+      let section = self.numberOfSections > 1 ? indexPath.section + (indexPath.item + 1) / self.numberOfItems : 0
+      let item = (indexPath.item + 1) % self.numberOfItems
+      return self.collectionViewLayout.contentOffset(for: IndexPath(item: item, section: section))
+    }()
+    
+    self.collectionView.setContentOffset(contentOffset, animated: true)
   }
 }
 
 extension VMBannerView: UICollectionViewDataSource {
   
+  public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return self.numberOfItems
+  }
+  
+  public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let dataSource = self.dataSource else {
+      fatalError("Please implement VMBannerViewDataSource protocol method")
+    }
+    
+    let index = indexPath.item
+    
+    self.dequeueSection = indexPath.section
+    
+    let cell = dataSource.bannerView(self, cellForItemAt: index)
+    
+    return cell
+  }
+  
+  public func numberOfSections(in collectionView: UICollectionView) -> Int {
+    guard let dataSource = self.dataSource else {
+      fatalError("Please implement VMBannerViewDataSource protocol method")
+    }
+    
+    self.numberOfItems = dataSource.numberOfItems(in: self)
+    guard self.numberOfItems > 0 else {
+      return 0
+    }
+    
+    self.numberOfSections = self.isInfinite && (self.numberOfItems > 1 || !self.removeInfiniteLoopForSingleItem) ? Int(Int16.max) / self.numberOfItems : 1
+    
+    return self.numberOfSections
+  }
 }
 
 extension VMBannerView: UICollectionViewDelegate {
   
+  public func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+    guard let function = self.delegate?.bannerView(_:shouldHighlightItemAt:) else {
+      return true
+    }
+    
+    let index = indexPath.item % self.numberOfItems
+    return function(self, index)
+  }
+  
+  public func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+    guard let function = self.delegate?.bannerView(_:didHighlightItemAt:) else {
+      return
+    }
+    let index = indexPath.item % self.numberOfItems
+    function(self,index)
+  }
+  
+  public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+    guard let function = self.delegate?.bannerView(_:shouldSelectItemAt:) else {
+      return true
+    }
+    
+    let index = indexPath.item % self.numberOfItems
+    return function(self, index)
+  }
+  
+  public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    guard let function = self.delegate?.bannerView(_:didSelectItemAt:) else {
+      return
+    }
+    
+    self.possibleTargetIndexPath = indexPath
+    defer {
+      self.possibleTargetIndexPath = nil
+    }
+    
+    let index = indexPath.item % self.numberOfItems
+    function(self,index)
+  }
+  
+  public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    guard let function = self.delegate?.bannerView(_:willDisplay:forItemAt:) else {
+      return
+    }
+    
+    let index = indexPath.item % self.numberOfItems
+    function(self, cell as! VMBannerViewItemProtocol , index)
+  }
+  
+  public func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    guard let function = self.delegate?.bannerView(_:didEndDisplaying:forItemAt:) else {
+      return
+    }
+    
+    let index = indexPath.item % self.numberOfItems
+    function(self, cell as! VMBannerViewItemProtocol , index)
+  }
+}
+
+extension VMBannerView: UIScrollViewDelegate {
+  
+  public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    if !self.isPossibleRotating && self.numberOfItems > 0 {
+      // In case someone is using KVO
+      let currentIndex = lround(Double(self.scrollOffset)) % self.numberOfItems
+      if currentIndex != self.currentIndex {
+        self.currentIndex = currentIndex
+      }
+    }
+    
+    guard let function = self.delegate?.bannerViewDidScroll(_:) else {
+      return
+    }
+    function(self)
+  }
+  
+  public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+    if let function = self.delegate?.bannerViewWillBeginDragging(_:) {
+      function(self)
+    }
+    if self.automaticSlidingInterval > 0 {
+      self.cancelTimer()
+    }
+  }
+  
+  public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    if let function = self.delegate?.bannerViewWillEndDragging(_:targetIndex:) {
+      let contentOffset = self.scrollDirection == .horizontal ? targetContentOffset.pointee.x : targetContentOffset.pointee.y
+      let targetIndex = lround(Double(contentOffset / self.collectionViewLayout.itemSpacing)) % self.numberOfItems
+      function(self, targetIndex)
+    }
+    
+    if self.automaticSlidingInterval > 0 {
+      self.startTimer()
+    }
+  }
+  
+  public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    if let function = self.delegate?.bannerViewDidEndDecelerating(_:) {
+      function(self)
+    }
+  }
+  
+  public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+    if let function = self.delegate?.bannerViewDidEndScrollingAnimation(_:) {
+      function(self)
+    }
+  }
 }
 
 extension VMBannerView {
@@ -197,4 +507,4 @@ extension VMBannerView {
   public static let automaticSize = CGSize.zero
 }
 
-
+#endif
